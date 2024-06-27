7e3471d530ba86c3eee27a533e224fa4b8d7be5c
Vladislav Rudenko <vladisrud@gmail.com>
2024-06-27 21:42:45 +0400

    Initial Commit

---
+++ ContactListUI/ContactListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUIApp.swift
+//  ContactListUI
+//
+//  Created by Влад Руденко on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
